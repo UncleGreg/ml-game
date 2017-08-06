7cda1224d55024da82efec486aa582b124d0304f
unclegreg <grzesiekwitczak@gmail.com>
2017-08-06 16:17:19 +0200

    shuffle numbers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import css from './index.styl';
 
+const numbers = Array.from({length: 100}, (v, k) => k+1); 
+
+function shuffle(array) {
+  var i = 0
+    , j = 0
+    , temp = null
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1))
+      temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+    }
+};
 
 function generateNumbers() {
-  for (var i = 1; i < 101; i++){
+  shuffle(numbers);
+  for (var i = 0; i < 100; i++){
     var x = document.createElement("button");
-    var t = document.createTextNode(i);
-    x.appendChild(t).id = i;
+    var t = document.createTextNode(numbers[i]);
+    x.appendChild(t);
+    x.setAttribute("id", "button-" + numbers[i]);
     document.getElementById('buttons-wrap').appendChild(x);}
 };
 
